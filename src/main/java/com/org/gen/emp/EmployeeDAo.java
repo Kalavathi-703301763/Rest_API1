5d4f30d5574510b4d01a4cd17710d3477c67afc9
kala <kalavathirs101@gmail.com>
2022-01-15 13:31:55 +0530

    servlet

---
+++ src/main/java/com/org/gen/emp/EmployeeDAo.java
@@ -0,0 +1,28 @@
+package com.org.gen.emp;
+
+import org.springframework.stereotype.Repository;
+import com.org.gen.emp.Employees;
+import com.org.gen.emp.Employee;
+
+
+
+@Repository
+public class EmployeeDAo {
+
+
+
+private static Employees list = new Employees();
+
+static {
+list.getEmployeeList().add(new Employee(1,"a","b","xyz"));
+list.getEmployeeList().add(new Employee(2,"p","q","sdyz"));
+
+}
+public Employees getAllEmployees() {
+return list;
+}
+public void addEmployee(Employee employee) {
+list.getEmployeeList().add(employee);
+}
+}
+
---
+++ src/main/java/com/org/gen/emp/Employees.java
@@ -0,0 +1,24 @@
+package com.org.gen.emp;
+
+import java.util.List;
+import java.util.ArrayList;
+public class Employees {
+
+
+
+private List<Employee> employeeList;
+
+//return list
+public List<Employee> getEmployeeList(){
+if(employeeList == null) {
+employeeList=new ArrayList<>();
+}
+return employeeList;
+
+}
+public void setEmployeeList(List<Employee> employeeList) {
+this.employeeList=employeeList;
+}
+
+
+}
